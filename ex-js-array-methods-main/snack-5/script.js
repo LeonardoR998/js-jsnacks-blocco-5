49ab74645cb364a2b413fb9ac87bd49200211409
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-05 14:40:44 +0100

    add solutions of 5-7 snacks

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,7 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const evenNumbers = nums.filter((num) => num % 2 === 0);
+
 // Risultato: [2, 8, 4, 12]
+console.log(evenNumbers);
