fc5560a01e0bbe8d92762ccd3d2f33428df8f9b8
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-05 14:08:06 +0100

    add solutions of 1-4 snacks

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,7 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -1,8 +1,11 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
+console.log(people[0]);
+console.log(people[1]);
+console.log(people[2]);
 // Risultato: 'Paolo', 'Giulia', 'Marco'
